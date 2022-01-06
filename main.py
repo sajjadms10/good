20e1ef2b1622a9ee55d7f68e1172866fba607db1
sajjadms10 <sajjadms10@gmail.com>
2022-01-06 13:47:05 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+print("hello")
+
+number=[9,0,4,8,9,6,0,6,6,9]
+print(number[1])
+
+
+for i in range(10):
+    print("mohammed sajajd m")
+    for j in range(2):
+        print("very good")
+        print(number[i])
+
